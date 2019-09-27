5bdf4a9dd9a7b7420012ddf0854dad1ec6f6c001
quynhntv <quynhntv@vnext.com.vn>
2019-09-27 17:41:40 +0700

    hw for student

---
+++ level1/lesson2/hw/hw_4.py
@@ -0,0 +1,13 @@
+# print(" Mời các em nhập 1 số bất kỳ: ")
+# s = int(input())
+#
+# s = input()
+# print("Các em đã nhập:", s)
+# print(type(s))
+# Nhập từ bàn phím 2 số a và b kiểu int, sau đó tính và in ra tổng, hiệu, tích, thương của 2 số đó
+a = int(input("Mời các em nhập số a: "))
+b = int(input("Mời các em nhập số b:" ))
+print("Tổng của a và b là", a + b )
+print("Hiệu của a và b là", a - b )
+print("Tích của a và b là", a * b )
+print("Thương của a và b là", a / b )
