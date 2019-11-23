e2acee906e5afe8e09fcc1ba73e798b1e4b0b2e8
quynh.vi <quynhntc@vnext.com.vn>
2019-11-23 08:28:43 +0700

    Merge branch 'master' of E:\teky with conflicts.

---
+++ level1/lesson8/hw/liem.py
@@ -0,0 +1,28 @@
+#Bai 1
+i1=(input('Moi nhap vao mot cau bat ki:'))
+t=len(i1)
+print(i1)
+print(t)
+#Bai 3
+i3=input('Moi nhap vao mot cau bat ki:')
+print(i3.split)
+#Bai 4
+i4=input('Moi nhap vao mot cau bat ki')
+n=input('Moi nhap mot ky tu cua cau bat ki')
+print(i4.find(n))
+#Bai 5
+i5=input('Moi nhap vao mot cau bat ki:')
+n=len(i5)
+print(i5[n:0])
+#Bai 6
+
+#Nhap ten
+ten=input('Moi thi sinh nhap ten nhap ten cua minh:')
+#Chao mung
+print('Chao mung thi sinh da den voi chiec non ki dieu')
+#Hien thi ra o chu va chu de cua o chu
+o_chu='Mua Xuan'
+print('O chu hom nay gom 7 chu cai, voi chu de mot mua trong nam')
+#Quay non
+lenh1=input('Truoc khi bat dau, moi nguoi choi quay non(nhap quay tu ban phim)')
+    # if lenh1='quay':
---
+++ level1/lesson9/__init__.py
@@ -0,0 +1,4 @@
+# day = ["monday", "tuesday", "wednesday", "thursday"]
+list=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+n = list[4]
+print(list[4])
